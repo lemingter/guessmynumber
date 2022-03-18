4a14f0ee4a89f65b1888e6a35ea52e8e25934895
lemingter <leetv2.1@gmail.com>
2022-03-18 13:34:14 -0600

    Falta texto

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,22 +5,43 @@ class Game extends Component {
     constructor() {
         super();
         this.state = {
-            number: 0,
-            message: "",
+            number: "",
+            message: "Hola",
             random: generateRandomNumber(100)
         }
     }
 
+    handleOnChange = e => {
+        const {target: {value}} = e;
+        console.log(value);
+        if(value > 0) {
+            this.setState({number: value});
+        }
+    }
+
+    handleOnClick = () => {
+        const number = parseInt(this.state.number);
+        const random = parseInt(this.state.random);
+
+        const text = calculateText(number, random);
+
+        this.setState({
+            message: text
+        })
+    }
+
     render() {
+        console.log(this.state.random);
         return (
             <div className="Game">
                 <input 
                     type="number"
-
+                    value={this.state.number}
+                    onChange = {this.handleOnChange}
                 />
 
-                <button>Probar</button>
-                <p>Texto</p>
+                <button onclick = {this.handleOnClick}>Probar</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
@@ -30,4 +51,10 @@ export default Game;
 
 function generateRandomNumber(max, min = 1) {
     return Math.floor(Math.random() *(max - min) + min);
+}
+
+function calculateText(number, random) {
+    if(number === random) {
+        return "Felicidades acertaste";
+    }
 }
